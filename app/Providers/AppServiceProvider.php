d13356f9cd7647d4c7c0ef4db51ce700858d6e05
Reinert Yosua <>
2024-05-09 11:08:01 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        if(this->app->environment('production')){
+            URL::forceScheme('https');
+        };
+
         Paginator::useBootstrapFive();
     }
 }
